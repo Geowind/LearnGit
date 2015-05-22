10823230561e1a1cd8e1b960a543f89c1d141e88
Dangerous <857911782@qq.com>
2015-05-22 22:43:53 +0800

    from ls-4

--- src/cn/geowind/learngit/MainActivity.java
+++ src/cn/geowind/learngit/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+       return false;
     }
 
     @Override
