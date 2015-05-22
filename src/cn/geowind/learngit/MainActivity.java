4d9229e4dc8f0862b95684b10d03d6d51e309834
Dangerous <857911782@qq.com>
2015-05-22 22:49:38 +0800

    from ls-5

--- src/cn/geowind/learngit/MainActivity.java
+++ src/cn/geowind/learngit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main12);
+        setContentView(R.layout.activity_main13);
     }
 
 
