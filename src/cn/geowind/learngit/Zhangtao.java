04ccb0b09d4e056f6aa7fcc41a0482c8024b624d
zhangtao <962076337@qq.com>
2015-05-24 00:13:45 +0800

    change Zhangtao.java

--- src/cn/geowind/learngit/Zhangtao.java
+++ src/cn/geowind/learngit/Zhangtao.java
@@ -1,2 +1,8 @@
-class Zhangtao{
-}
+class Zhangtao extends JFrame{
+ public void main(a0[]){
+ 	this.setTitle("уелн");
+	this.sestSize(500,500);
+	this.setLocationRelativeTo(null);
+	this.setVisible(true);
+ }
+}`
