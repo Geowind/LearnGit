f752c11bb9f60e0dcde3ffd5223348352e0d4049
zhangtao <962076337@qq.com>
2015-05-24 00:02:10 +0800

    change MainActivity.java 22

--- src/cn/geowind/learngit/MainActivity.java
+++ src/cn/geowind/learngit/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main20);
+        setContentView(R.layout.activity_main22);
+	//I modified it --by zt
     }
 
 
