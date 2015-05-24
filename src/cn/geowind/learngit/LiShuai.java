ff5e51f8ba262ce4ab77c9658b23063989f2c1e5
Dangerous <857911782@qq.com>
2015-05-24 08:58:17 +0800

    commit from git with eclipse

--- src/cn/geowind/learngit/LiShuai.java
+++ src/cn/geowind/learngit/LiShuai.java
@@ -2,6 +2,6 @@ package cn.geowind.learngit;
 
 public class LiShuai{
   public static void main(String[] args){
-    System.out.println("My name is LiShuai!");
+    System.out.println("My name is LiShuai! Modify from git with eclipse");
   }
 }
